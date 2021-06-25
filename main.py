e7ad6dae65e8d5c2c217159c48c5e16b043597bb
unknown <iarafat386@gmail.com>
2021-06-25 13:00:00 +0600

    PyPDF2 Installed

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pyttsx3
+import PyPDF2
+
+book = open('oop.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
 friend = pyttsx3.init()
-friend.say('I can speak now anything you want me to speak babe.')
+page = pdfReader.getPage(7)
+text = page.extractText()
+friend.say(text)
 friend.runAndWait()
